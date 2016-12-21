6debf3de36e2583beab6ab49ea19d2288dddc117
TylerYao0407 <694721975@qq.com>
2016-12-21 15:58:44 +0800

    Initial commit

---
+++ src/main/java/com/tyler/dao/UserMapper.java
@@ -0,0 +1,55 @@
+package com.tyler.dao;
+
+import com.tyler.model.User;
+import org.apache.ibatis.annotations.Arg;
+import org.apache.ibatis.annotations.ConstructorArgs;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.InsertProvider;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+import org.apache.ibatis.annotations.UpdateProvider;
+import org.apache.ibatis.type.JdbcType;
+
+public interface UserMapper {
+    @Delete({
+        "delete from user",
+        "where id = #{id,jdbcType=INTEGER}"
+    })
+    int deleteByPrimaryKey(Integer id);
+
+    @Insert({
+        "insert into user (id, name, ",
+        "age)",
+        "values (#{id,jdbcType=INTEGER}, #{name,jdbcType=VARCHAR}, ",
+        "#{age,jdbcType=INTEGER})"
+    })
+    int insert(User record);
+
+    @InsertProvider(type=UserSqlProvider.class, method="insertSelective")
+    int insertSelective(User record);
+
+    @Select({
+        "select",
+        "id, name, age",
+        "from user",
+        "where id = #{id,jdbcType=INTEGER}"
+    })
+    @ConstructorArgs({
+        @Arg(column="id", javaType=Integer.class, jdbcType=JdbcType.INTEGER, id=true),
+        @Arg(column="name", javaType=String.class, jdbcType=JdbcType.VARCHAR),
+        @Arg(column="age", javaType=Integer.class, jdbcType=JdbcType.INTEGER)
+    })
+    User selectByPrimaryKey(Integer id);
+
+    @UpdateProvider(type=UserSqlProvider.class, method="updateByPrimaryKeySelective")
+    int updateByPrimaryKeySelective(User record);
+
+    @Update({
+        "update user",
+        "set name = #{name,jdbcType=VARCHAR},",
+          "age = #{age,jdbcType=INTEGER}",
+        "where id = #{id,jdbcType=INTEGER}"
+    })
+    int updateByPrimaryKey(User record);
+}
